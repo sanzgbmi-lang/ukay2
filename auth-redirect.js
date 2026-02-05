33e4968dea6ccc63ecf4f6970f75c635222e3fa9
sanzgbmi-lang <sanzgbmi@gmail.com>
2026-02-05 17:06:23 +0800

    Update auth-redirect.js

--- auth-redirect.js
+++ auth-redirect.js
@@ -1,11 +1,4 @@
-const SUPABASE_URL = "https://ozqyemgpjtvijnwdaemq.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cXllbWdwanR2aWpud2RhZW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1ODY4ODYsImV4cCI6MjA4NTE2Mjg4Nn0.SQ_BpsgSQiTDGczS6oSXaDAXILTfghYvtGAm1fSKtCQ";
-
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+const supabaseClient = window.supabaseClient;
 
 async function redirectIfLoggedIn() {
   const { data: { user } } = await supabaseClient.auth.getUser();
@@ -16,3 +9,4 @@ async function redirectIfLoggedIn() {
 }
 
 redirectIfLoggedIn();
+
