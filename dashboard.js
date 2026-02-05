28bf01c9cf63422871ec9a6d1ae0587ff83227b6
sanzgbmi-lang <sanzgbmi@gmail.com>
2026-02-05 16:56:04 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,6 @@
-const SUPABASE_URL = "https://ozqyemgpjtvijnwdaemq.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cXllbWdwanR2aWpud2RhZW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1ODY4ODYsImV4cCI6MjA4NTE2Mjg4Nn0.SQ_BpsgSQiTDGczS6oSXaDAXILTfghYvtGAm1fSKtCQ";
-
 const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  window.SUPABASE_CONFIG.url,
+  window.SUPABASE_CONFIG.anonKey
 );
 
 async function loadProfile() {
