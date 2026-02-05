4094799c74096301dc7584c9ae49cf2493c0f322
sanzgbmi-lang <sanzgbmi@gmail.com>
2026-02-05 16:56:22 +0800

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://ozqyemgpjtvijnwdaemq.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cXllbWdwanR2aWpud2RhZW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1ODY4ODYsImV4cCI6MjA4NTE2Mjg4Nn0.SQ_BpsgSQiTDGczS6oSXaDAXILTfghYvtGAm1fSKtCQ"
+};
