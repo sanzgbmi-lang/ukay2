dce2424f36a45b7ab2a841279f9e398a4aed97e7
sanzgbmi-lang <sanzgbmi@gmail.com>
2026-02-05 16:48:54 +0800

    Add files via upload

---
+++ auth-redirect.js
@@ -0,0 +1,18 @@
+const SUPABASE_URL = "https://ozqyemgpjtvijnwdaemq.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cXllbWdwanR2aWpud2RhZW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1ODY4ODYsImV4cCI6MjA4NTE2Mjg4Nn0.SQ_BpsgSQiTDGczS6oSXaDAXILTfghYvtGAm1fSKtCQ";
+
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+async function redirectIfLoggedIn() {
+  const { data: { user } } = await supabaseClient.auth.getUser();
+
+  if (user) {
+    window.location.replace("home.html");
+  }
+}
+
+redirectIfLoggedIn();
