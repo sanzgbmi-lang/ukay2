7baf6a781029871dd94773f8daf0e0c330c43ffe
sanzgbmi-lang <sanzgbmi@gmail.com>
2026-02-05 17:06:57 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,10 +1,4 @@
-const SUPABASE_URL = "https://ozqyemgpjtvijnwdaemq.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96cXllbWdwanR2aWpud2RhZW1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1ODY4ODYsImV4cCI6MjA4NTE2Mjg4Nn0.SQ_BpsgSQiTDGczS6oSXaDAXILTfghYvtGAm1fSKtCQ";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+const supabaseClient = window.supabaseClient;
 
 async function protectPage() {
   const { data: { user } } = await supabaseClient.auth.getUser();
@@ -20,3 +14,4 @@ async function protectPage() {
 }
 
 protectPage();
+
